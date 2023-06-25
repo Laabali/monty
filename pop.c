d11326cf8c65d9e643a451de71c0f6bce0b7482c
laabali <khalidlaabali@gmail.com>
2023-06-25 03:36:19 -0700

    Add f_pop function to monty.h

--- pop.c
+++ pop.c
@@ -1,23 +1,18 @@
 #include "monty.h"
-/**
- * f_pop - prints the top
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-void f_pop(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
 
-	if (*head == NULL)
-	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	h = *head;
-	*head = h->next;
-	free(h);
+/**
+ * pop - removes the top element of the stack
+ * @top: pointer to the head of the stack
+ * @line_number: line number of the command being run
+ */
+void pop(stack_t **top, uint line_number)
+{
+	stack_t *temp;
+
+	if (*top == NULL)
+		pop_err(line_number);
+
+	temp = *top;
+	*top = (*top)->next;
+	free(temp);
 }
