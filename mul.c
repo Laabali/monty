725e59cd88e77904137dfd350b2fdf82ce8cd5ae
laabali <khalidlaabali@gmail.com>
2023-06-25 03:24:24 -0700

    Add f_mul function to monty.h

--- mul.c
+++ mul.c
@@ -1,32 +1,20 @@
 #include "monty.h"
-/**
- * f_mul - multiplies the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-void f_mul(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	int len = 0, aux;
 
-	h = *head;
-	while (h)
-	{
-		h = h->next;
-		len++;
-	}
-	if (len < 2)
-	{
-		fprintf(stderr, "L%d: can't mul, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	h = *head;
-	aux = h->next->n * h->n;
-	h->next->n = aux;
-	*head = h->next;
-	free(h);
+/**
+ * mul - multiplies the top two elements of the stack
+ * @top: pointer to the head of the stack
+ * @line_number: line number of the command being run
+ */
+void mul(stack_t **top, ui line_number)
+{
+	stack_t *tmp;
+
+	tmp = *top;
+
+	if (!tmp || !tmp->next)
+		short_stack_err("mul", line_number);
+	tmp->next->n *= tmp->n;
+	*top = tmp->next;
+
+	free(tmp);
 }
