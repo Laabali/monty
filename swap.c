de66e2643f3e6e9d8c8efc017337d19d341a719e
laabali <khalidlaabali@gmail.com>
2023-06-25 03:49:14 -0700

    Add f_swap function to monty.h

--- swap.c
+++ swap.c
@@ -1,31 +1,18 @@
 #include "monty.h"
-/**
- * f_swap - adds the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-void f_swap(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	int len = 0, aux;
 
-	h = *head;
-	while (h)
-	{
-		h = h->next;
-		len++;
-	}
-	if (len < 2)
-	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	h = *head;
-	aux = h->n;
-	h->n = h->next->n;
-	h->next->n = aux;
+/**
+ * swap - swaps the top two elements of a stack
+ * @top: pointer to the head of the stack
+ * @line_number: line number of the command being run
+ */
+void swap(stack_t **top, uint line_number)
+{
+	int tmp;
+
+	if (!*top || !(*top)->next)
+		short_stack_err("swap", line_number);
+
+	tmp = (*top)->n;
+	(*top)->n = (*top)->next->n;
+	(*top)->next->n = tmp;
 }
