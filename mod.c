589e8622ade9c74bdcaa4199136ff17ba4aa2804
laabali <khalidlaabali@gmail.com>
2023-06-25 03:17:23 -0700

    Add f_mod function to monty.h

--- mod.c
+++ mod.c
@@ -1,41 +1,21 @@
 #include "monty.h"
-/**
- * f_mod - computes the rest of the division of the second
- * top element of the stack by the top element of the stack
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-void f_mod(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	int len = 0, aux;
 
-	h = *head;
-	while (h)
-	{
-		h = h->next;
-		len++;
-	}
-	if (len < 2)
-	{
-		fprintf(stderr, "L%d: can't mod, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	h = *head;
-	if (h->n == 0)
-	{
-		fprintf(stderr, "L%d: division by zero\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	aux = h->next->n % h->n;
-	h->next->n = aux;
-	*head = h->next;
-	free(h);
+/**
+ * mod - Computes the remainder of the second top element of the stack
+ * @top: pointer to the head of the stack
+ * @line_number: line number of the command being run
+ */
+void mod(stack_t **top, ui line_number)
+{
+	stack_t *tmp;
+
+	tmp = *top;
+
+	if (!tmp || !tmp->next)
+		short_stack_err("mod", line_number);
+
+	tmp->next->n %= tmp->n;
+	*top = tmp->next;
+
+	free(tmp);
 }
