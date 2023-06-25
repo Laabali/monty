a7bc57470aa62a059712e6b2c1f0d16b6f3d7c10
laabali <khalidlaabali@gmail.com>
2023-06-25 03:46:00 -0700

    Add f_sub function to monty.h

--- sub.c
+++ sub.c
@@ -1,29 +1,19 @@
 #include "monty.h"
-/**
-  *f_sub- sustration
-  *@head: stack head
-  *@counter: line_number
-  *Return: no return
- */
-void f_sub(stack_t **head, unsigned int counter)
-{
-	stack_t *aux;
-	int sus, nodes;
 
-	aux = *head;
-	for (nodes = 0; aux != NULL; nodes++)
-		aux = aux->next;
-	if (nodes < 2)
-	{
-		fprintf(stderr, "L%d: can't sub, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	aux = *head;
-	sus = aux->next->n - aux->n;
-	aux->next->n = sus;
-	*head = aux->next;
-	free(aux);
+/**
+ * sub - subtracts the top two elements of the stack
+ * @top: pointer to the head of the stack
+ * @line_number: line number of the command being run
+ */
+void sub(stack_t **top, ui line_number)
+{
+	stack_t *tmp;
+
+	if (!*top || !(*top)->next)
+		short_stack_err("sub", line_number);
+
+	(*top)->next->n -= (*top)->n;
+	tmp = *top;
+	*top = (*top)->next;
+	free(tmp);
 }
