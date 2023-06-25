57a6d5088c0566c0b247023bf203811b7642ba41
laabali <khalidlaabali@gmail.com>
2023-06-25 03:02:11 -0700

    Add f_errors1 function to monty.h

--- div.c
+++ div.c
@@ -1,40 +1,24 @@
 #include "monty.h"
-/**
- * f_div - divides the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-void f_div(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	int len = 0, aux;
 
-	h = *head;
-	while (h)
-	{
-		h = h->next;
-		len++;
-	}
-	if (len < 2)
-	{
-		fprintf(stderr, "L%d: can't div, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	h = *head;
-	if (h->n == 0)
-	{
-		fprintf(stderr, "L%d: division by zero\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	aux = h->next->n / h->n;
-	h->next->n = aux;
-	*head = h->next;
-	free(h);
+/**
+ * _div - divides the second top element of the stack from the top element
+ * @top: pointer to the head of the stack
+ * @line_number: line number of the command being run
+ */
+void _div(stack_t **top, ui line_number)
+{
+	stack_t *tmp;
+
+	tmp = *top;
+
+	if (!tmp || !tmp->next)
+		short_stack_err("div", line_number);
+
+	if (tmp->n == 0)
+		div_by_zero_err(line_number);
+
+	tmp->next->n /= tmp->n;
+	*top = tmp->next;
+
+	free(tmp);
 }
